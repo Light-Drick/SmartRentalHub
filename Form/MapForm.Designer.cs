02ff62e74b5531db9a6128391034c6e4f083317d
Light-Drick <149278755+Light-Drick@users.noreply.github.com>
2023-12-11 17:16:58 +0800

    Show pin longt and lat in the home form map

--- Form/MapForm.Designer.cs
+++ Form/MapForm.Designer.cs
@@ -28,25 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.splitter1 = new System.Windows.Forms.Splitter();
             this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
             this.label1 = new System.Windows.Forms.Label();
-            this.LatitudeTbx = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.LongitudeTbx = new System.Windows.Forms.TextBox();
-            this.LoadMapBtn = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.NameTbx = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // splitter1
-            // 
-            this.splitter1.Location = new System.Drawing.Point(0, 0);
-            this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(144, 479);
-            this.splitter1.TabIndex = 3;
-            this.splitter1.TabStop = false;
             // 
             // gMapControl1
             // 
@@ -74,6 +61,7 @@
             this.gMapControl1.TabIndex = 4;
             this.gMapControl1.Zoom = 0D;
             this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
+            this.gMapControl1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gMapControl1_MouseClick);
             // 
             // label1
             // 
@@ -83,80 +71,40 @@
             this.label1.Size = new System.Drawing.Size(112, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Google map here";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // LatitudeTbx
+            // richTextBox1
             // 
-            this.LatitudeTbx.Location = new System.Drawing.Point(23, 221);
-            this.LatitudeTbx.Name = "LatitudeTbx";
-            this.LatitudeTbx.Size = new System.Drawing.Size(100, 22);
-            this.LatitudeTbx.TabIndex = 5;
+            this.richTextBox1.Location = new System.Drawing.Point(24, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(137, 256);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 355);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(20, 202);
+            this.label2.Location = new System.Drawing.Point(12, 336);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 16);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Latitude";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(20, 273);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(66, 16);
-            this.label3.TabIndex = 8;
-            this.label3.Text = "Longitude";
-            // 
-            // LongitudeTbx
-            // 
-            this.LongitudeTbx.Location = new System.Drawing.Point(23, 292);
-            this.LongitudeTbx.Name = "LongitudeTbx";
-            this.LongitudeTbx.Size = new System.Drawing.Size(100, 22);
-            this.LongitudeTbx.TabIndex = 7;
-            // 
-            // LoadMapBtn
-            // 
-            this.LoadMapBtn.Location = new System.Drawing.Point(23, 341);
-            this.LoadMapBtn.Name = "LoadMapBtn";
-            this.LoadMapBtn.Size = new System.Drawing.Size(100, 23);
-            this.LoadMapBtn.TabIndex = 9;
-            this.LoadMapBtn.Text = "Load Map";
-            this.LoadMapBtn.UseVisualStyleBackColor = true;
-            this.LoadMapBtn.Click += new System.EventHandler(this.LoadMapBtn_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 11);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(47, 16);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Name:";
-            // 
-            // NameTbx
-            // 
-            this.NameTbx.Location = new System.Drawing.Point(12, 30);
-            this.NameTbx.Name = "NameTbx";
-            this.NameTbx.Size = new System.Drawing.Size(100, 22);
-            this.NameTbx.TabIndex = 10;
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
             // 
             // MapForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(865, 479);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.NameTbx);
-            this.Controls.Add(this.LoadMapBtn);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.LongitudeTbx);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.LatitudeTbx);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.gMapControl1);
-            this.Controls.Add(this.splitter1);
             this.Controls.Add(this.label1);
             this.Name = "MapForm";
             this.Text = "MapForm";
@@ -167,15 +115,10 @@
         }
 
         #endregion
-        private System.Windows.Forms.Splitter splitter1;
         private GMap.NET.WindowsForms.GMapControl gMapControl1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox LatitudeTbx;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox LongitudeTbx;
-        private System.Windows.Forms.Button LoadMapBtn;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox NameTbx;
     }
 }
--- Form/MapForm.cs
+++ Form/MapForm.cs
@@ -12,8 +12,11 @@ using GMap.NET;
 using GMap.NET.MapProviders;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
+using Google.Cloud.Firestore;
+using Google.Cloud.Location;
+using Newtonsoft.Json;
 using OpenCage.Geocode;
-
+using SmartRentalHub.Class;
 
 namespace SmartRentalHub
 {
@@ -25,90 +28,173 @@ namespace SmartRentalHub
         private GeocodingHelper geocodingHelper;
         Geocoder geocoder = new Geocoder("26abc1c4541f47ba95cf68789121f788");
         private readonly List<string> suggestedSearches = new List<string>();
-
+        
         public MapForm()
         {
             InitializeComponent();
-            
-            GMapProviders.GoogleMap.ApiKey = @"AIzaSyA_ltg1q16Xf_cSIw7S64HDMQ_hAUDU2cI";
-            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+
+            //GMapProviders.GoogleMap.ApiKey = @"AIzaSyA_ltg1q16Xf_cSIw7S64HDMQ_hAUDU2cI";
+            // gMapControl1.MapProvider = GMapProviders.GoogleMap;
+
+            //firestore connection
+            FirestoreHelper.SetEnvironmentVariable();
 
 
 
         }
 
-        private void MapForm_Load(object sender, EventArgs e)
+
+        private async void MapForm_Load(object sender, EventArgs e)
         {
+            try
+            {
+                zooming();
+                // Initialize the map
+                GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
+                gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+                gMapControl1.Position = new PointLatLng(10.31672, 123.89071);
+                gMapControl1.ShowCenter = false;
 
-            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
-            gMapControl1.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance;
+                // Fetch locations from Firestore and add them to the map
+                await GetLocationsFromFirestore();
 
-            // Use new PointLatLng constructor to set the initial position
-            gMapControl1.Position = new PointLatLng(10.294154847289409, 123.88125871906306);
-
-            // Set a default zoom level
-            gMapControl1.Zoom = 20;
-
-            markersOverlay = new GMap.NET.WindowsForms.GMapOverlay("markers");
-            gMapControl1.Overlays.Add(markersOverlay);
-
-            // Initialize the geocoding helper
-            geocodingHelper = new GeocodingHelper();
+                Get_All_Documents_Lat_Longt_From_Collection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error in MapForm_Load: {ex.Message}");
+            }
         }
 
-        private async void LoadMapBtn_Click(object sender, EventArgs e)
-        {
-
-            string placeName = NameTbx.Text.Trim();
-            var coordinates = await geocodingHelper.GetCoordinatesAsync(placeName);
-
-            // Check if there are existing markers, remove them if any
-            if (markersOverlay.Markers.Count > 0)
-            {
-                markersOverlay.Markers.Clear();
-            }
-            if (gMapControl1.Overlays.Any(o => o.Id == "O"))
-            {
-                GMapOverlay existingOverlay = gMapControl1.Overlays.Last(o => o.Id == "O");
-                existingOverlay.Markers.Clear();
-            }
-
-            // Add a marker to the map at the specified location
-            GMapMarker marker = new GMarkerGoogle(new GMap.NET.PointLatLng(coordinates.Lat, coordinates.Lng), GMarkerGoogleType.red_pushpin);
-            markersOverlay.Markers.Add(marker);
-
-            // Set the map center and zoom to the marker
-            gMapControl1.Position = marker.Position;
-            gMapControl1.Zoom = 15;
-
-            // Refresh the map
-            gMapControl1.Refresh();
-            LatitudeTbx.Text = Convert.ToString(coordinates.Lat);
-            LongitudeTbx.Text = Convert.ToString(coordinates.Lng);
-
-        }
 
         private void zooming()
         {
             // Set the zoom level and max zoom level
-            gMapControl1.Zoom = 10; // You can adjust this value as needed
-            gMapControl1.MaxZoom = 18; // You can adjust this value as needed
+            gMapControl1.Zoom = 15; // You can adjust this value as needed
+            gMapControl1.MaxZoom = 100; // You can adjust this value as needed
 
             // Enable zooming by scroll
             gMapControl1.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
 
             // Enable dragging the map with the Left mouse button
             gMapControl1.DragButton = MouseButtons.Left;
+
+            //zoom in at the cursor
+            gMapControl1.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
+        }
+
+
+        public async Task GetLocationsFromFirestore()
+        {
+            List<PointLatLng> locations = new List<PointLatLng>();
+
+            try
+            {
+                CollectionReference spaceForRentRef = FirestoreHelper.database.Collection("Space for rent");
+                QuerySnapshot ownersSnapshot = await spaceForRentRef.GetSnapshotAsync();
+
+                foreach (DocumentSnapshot ownerSnapshot in ownersSnapshot.Documents)
+                {
+                    CollectionReference roomsRef = spaceForRentRef.Document(ownerSnapshot.Id).Collection("Rooms");
+                    QuerySnapshot roomsSnapshot = await roomsRef.GetSnapshotAsync();
+
+                    foreach (DocumentSnapshot roomSnapshot in roomsSnapshot.Documents)
+                    {
+                        Dictionary<string, object> roomData = roomSnapshot.ToDictionary();
+                        if (roomData.ContainsKey("Latitude") && roomData.ContainsKey("Longitude"))
+                        {
+                            double lat, lng;
+                            if (double.TryParse(roomData["Latitude"].ToString(), out lat) && double.TryParse(roomData["Longitude"].ToString(), out lng))
+                            {
+                                locations.Add(new PointLatLng(lat, lng));
+
+                            }
+                        }
+                    }
+                }
+
+                AddPinsToMap(locations);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+        public void AddPinsToMap(List<PointLatLng> locations)
+        {
+            MessageBox.Show($"Number of locations to add: {locations.Count}");
+            var markers = new GMapOverlay("markers");
+            foreach (var location in locations)
+            {
+                var marker = new GMarkerGoogle(location, GMarkerGoogleType.red_pushpin);
+                markers.Markers.Add(marker);
+                
+            }
+            gMapControl1.Overlays.Add(markers);
+            gMapControl1.Refresh();
+        }
+
+        private void gMapControl1_MouseClick(object sender, MouseEventArgs e)
+        {
+            gMapControl1.DragButton = MouseButtons.Right;
+
         }
 
         private void gMapControl1_Load(object sender, EventArgs e)
         {
-
+            zooming();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+
+        private async void Get_All_Documents_Lat_Longt_From_Collection()
         {
+            // Get a reference to the Firestore database
+            CollectionReference spaceForRentRef = FirestoreHelper.database.Collection("Space for rent");
 
+            // Get all documents (i.e., all owners) in the "Space for rent" collection
+            QuerySnapshot ownersSnapshot = await spaceForRentRef.GetSnapshotAsync();
+
+            foreach (DocumentSnapshot ownerSnapshot in ownersSnapshot)
+            {
+                if (ownerSnapshot.Exists)
+                {
+                    // Get the "Rooms" collection for this owner
+                    CollectionReference roomsRef = ownerSnapshot.Reference.Collection("Rooms");
+
+                    // Get all documents (i.e., all rooms) in this owner's "Rooms" collection
+                    QuerySnapshot roomsSnapshot = await roomsRef.GetSnapshotAsync();
+
+                    foreach (DocumentSnapshot roomSnapshot in roomsSnapshot)
+                    {
+                        if (roomSnapshot.Exists)
+                        {
+                            SpaceDetails spaceDetails = roomSnapshot.ConvertTo<SpaceDetails>();
+                            // Debugging lines
+                            Console.WriteLine("Document ID: " + roomSnapshot.Id);
+                            Console.WriteLine("SpaceDetails object: " + JsonConvert.SerializeObject(spaceDetails));
+
+                            richTextBox1.Text += "Name/Title of Space: " + roomSnapshot.Id + "\n";
+                            richTextBox1.Text += "Latitude: " + spaceDetails.Latitude + "\n";
+                            richTextBox1.Text += "Longitude: " + spaceDetails.Longitude + "\n\n";
+                            richTextBox1.Text += "Address: " + spaceDetails.Address + "\n\n";
+                            richTextBox1.Text += "Accommodation: " + spaceDetails.Accommodation + "\n\n";
+                            richTextBox1.Text += "Room Type: " + spaceDetails.RoomType + "\n\n";
+                            richTextBox1.Text += "Guests: " + spaceDetails.Guest + "\n\n";
+                            richTextBox1.Text += "Bed: " + spaceDetails.Bed + "\n\n";
+                            richTextBox1.Text += "Bedroom: " + spaceDetails.Bedroom + "\n\n";
+
+
+                        }
+                    }
+                }
+            }
+
+            MessageBox.Show("All Latitude and Longitude data retrieved successfully.");
         }
+
     }
 }
