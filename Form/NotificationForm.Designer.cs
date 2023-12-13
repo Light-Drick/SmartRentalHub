2081e9a1b9161c13ffdb5ee03803469d5f96e834
Light-Drick <149278755+Light-Drick@users.noreply.github.com>
2023-12-13 14:23:02 +0800

    Notification drafting

---
+++ Form/NotificationForm.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace SmartRentalHub
+{
+    partial class NotificationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BathroomsCmbx = new System.Windows.Forms.ComboBox();
+            this.label20 = new System.Windows.Forms.Label();
+            this.BedroomCmbx = new System.Windows.Forms.ComboBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.BedCmbx = new System.Windows.Forms.ComboBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.GuestCmbx = new System.Windows.Forms.ComboBox();
+            this.label18 = new System.Windows.Forms.Label();
+            this.RoomTypeCmbx = new System.Windows.Forms.ComboBox();
+            this.label17 = new System.Windows.Forms.Label();
+            this.AccomodationCmbx = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BathroomsCmbx
+            // 
+            this.BathroomsCmbx.FormattingEnabled = true;
+            this.BathroomsCmbx.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.BathroomsCmbx.Location = new System.Drawing.Point(420, 206);
+            this.BathroomsCmbx.Name = "BathroomsCmbx";
+            this.BathroomsCmbx.Size = new System.Drawing.Size(121, 24);
+            this.BathroomsCmbx.TabIndex = 70;
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(319, 209);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(75, 16);
+            this.label20.TabIndex = 75;
+            this.label20.Text = "Bathrooms:";
+            // 
+            // BedroomCmbx
+            // 
+            this.BedroomCmbx.FormattingEnabled = true;
+            this.BedroomCmbx.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.BedroomCmbx.Location = new System.Drawing.Point(420, 170);
+            this.BedroomCmbx.Name = "BedroomCmbx";
+            this.BedroomCmbx.Size = new System.Drawing.Size(121, 24);
+            this.BedroomCmbx.TabIndex = 67;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(319, 173);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(73, 16);
+            this.label21.TabIndex = 74;
+            this.label21.Text = "Bedrooms:";
+            // 
+            // BedCmbx
+            // 
+            this.BedCmbx.FormattingEnabled = true;
+            this.BedCmbx.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.BedCmbx.Location = new System.Drawing.Point(176, 206);
+            this.BedCmbx.Name = "BedCmbx";
+            this.BedCmbx.Size = new System.Drawing.Size(121, 24);
+            this.BedCmbx.TabIndex = 69;
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(75, 209);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(35, 16);
+            this.label19.TabIndex = 73;
+            this.label19.Text = "Bed:";
+            // 
+            // GuestCmbx
+            // 
+            this.GuestCmbx.FormattingEnabled = true;
+            this.GuestCmbx.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.GuestCmbx.Location = new System.Drawing.Point(176, 170);
+            this.GuestCmbx.Name = "GuestCmbx";
+            this.GuestCmbx.Size = new System.Drawing.Size(121, 24);
+            this.GuestCmbx.TabIndex = 66;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(75, 173);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(52, 16);
+            this.label18.TabIndex = 72;
+            this.label18.Text = "Guests:";
+            // 
+            // RoomTypeCmbx
+            // 
+            this.RoomTypeCmbx.FormattingEnabled = true;
+            this.RoomTypeCmbx.Items.AddRange(new object[] {
+            "Entire Place",
+            "Room",
+            "Shared Room"});
+            this.RoomTypeCmbx.Location = new System.Drawing.Point(420, 119);
+            this.RoomTypeCmbx.Name = "RoomTypeCmbx";
+            this.RoomTypeCmbx.Size = new System.Drawing.Size(121, 24);
+            this.RoomTypeCmbx.TabIndex = 65;
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(337, 127);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(82, 16);
+            this.label17.TabIndex = 71;
+            this.label17.Text = "Room Type:";
+            // 
+            // AccomodationCmbx
+            // 
+            this.AccomodationCmbx.FormattingEnabled = true;
+            this.AccomodationCmbx.Items.AddRange(new object[] {
+            "House",
+            "Apartment",
+            "Guest House",
+            "Hotel"});
+            this.AccomodationCmbx.Location = new System.Drawing.Point(176, 119);
+            this.AccomodationCmbx.Name = "AccomodationCmbx";
+            this.AccomodationCmbx.Size = new System.Drawing.Size(121, 24);
+            this.AccomodationCmbx.TabIndex = 64;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 68;
+            this.label2.Text = "Accomodation:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 65);
+            this.button1.TabIndex = 76;
+            this.button1.Text = "Notify me of new spaces that consist of these fields:";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 77;
+            this.label1.Text = "label1";
+            // 
+            // NotificationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 510);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BathroomsCmbx);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.BedroomCmbx);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.BedCmbx);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.GuestCmbx);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.RoomTypeCmbx);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.AccomodationCmbx);
+            this.Controls.Add(this.label2);
+            this.Name = "NotificationForm";
+            this.Text = "NotificationForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox BathroomsCmbx;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.ComboBox BedroomCmbx;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.ComboBox BedCmbx;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.ComboBox GuestCmbx;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.ComboBox RoomTypeCmbx;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.ComboBox AccomodationCmbx;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
