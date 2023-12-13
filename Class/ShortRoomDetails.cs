bc25a8b73c6c4d319f43f0559d45fa3ed56a23f8
Light-Drick <149278755+Light-Drick@users.noreply.github.com>
2023-12-14 07:41:37 +0800

    classes

---
+++ Class/ShortRoomDetails.cs
@@ -0,0 +1,28 @@
+﻿using Google.Cloud.Firestore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRentalHub
+{
+    [FirestoreData]
+    internal class ShortRoomDetails
+    {
+        [FirestoreProperty ]
+        public string Username { get; set; }
+
+        [FirestoreProperty]
+        public string PricePerNight { get; set; }
+
+        [FirestoreProperty]
+        public string NameTitleOfSpace { get; set; }
+
+        [FirestoreProperty]
+        public string RoomPic { get; set; }
+
+
+
+    }
+}
